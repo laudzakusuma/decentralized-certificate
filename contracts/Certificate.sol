63ad4932ab07e43ccdd1f17c0e79a2fe898f7415
laudzakusuma <laudzaxie@gmail.com>
2025-06-06 19:29:20 +0700

    contract

--- contracts/Certificate.sol
+++ contracts/Certificate.sol
@@ -1,10 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
 
-// Kita mengimpor kontrak dasar dari OpenZeppelin.
-// ERC721 adalah standar untuk NFT.
-// Ownable memberikan kontrol akses (hanya pemilik yang bisa melakukan aksi tertentu).
-// ERC721URIStorage memungkinkan kita menyimpan link metadata (URI) untuk setiap token.
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
@@ -16,7 +12,6 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
  * Hanya pemilik kontrak (institusi) yang dapat menerbitkan (mint) sertifikat baru.
  */
 contract Certificate is ERC721, ERC721URIStorage, Ownable {
-    // Counter untuk menghasilkan tokenId yang unik secara otomatis.
     uint256 private _nextTokenId;
 
     /**
@@ -32,15 +27,16 @@ contract Certificate is ERC721, ERC721URIStorage, Ownable {
      * @dev Fungsi untuk menerbitkan sertifikat baru.
      * Hanya bisa dipanggil oleh pemilik kontrak (institusi).
      * @param recipient Alamat wallet penerima sertifikat (mahasiswa).
-     * @param tokenURI Link ke metadata JSON yang disimpan di IPFS.
+     * @param _tokenURI Link ke metadata JSON yang disimpan di IPFS.
+     * * PERBAIKAN: Mengganti nama parameter 'tokenURI' menjadi '_tokenURI' untuk menghindari shadowing.
      */
-    function safeMint(address recipient, string memory tokenURI)
+    function safeMint(address recipient, string memory _tokenURI)
         public
         onlyOwner
     {
         uint256 tokenId = _nextTokenId++;
         _safeMint(recipient, tokenId);
-        _setTokenURI(tokenId, tokenURI);
+        _setTokenURI(tokenId, _tokenURI);
     }
 
     /**
@@ -52,19 +48,13 @@ contract Certificate is ERC721, ERC721URIStorage, Ownable {
      */
     function _update(address to, uint256 tokenId, address auth)
         internal
-        override(ERC721, ERC721URIStorage)
+        override(ERC721)
         returns (address)
     {
-        // Mendapatkan alamat pemilik saat ini dari token
         address from = _ownerOf(tokenId);
-
-        // Memastikan token hanya bisa ditransfer saat minting (dari alamat nol)
         require(from == address(0), "Certificate: This is a Soulbound token and cannot be transferred.");
-        
-        // Panggil fungsi _update dari parent contract untuk menyelesaikan proses minting
         return super._update(to, tokenId, auth);
     }
-
 
     // Fungsi berikut di-override dari ERC721URIStorage.
     // Kita perlu melakukannya karena Solidity membutuhkan kejelasan ketika ada
