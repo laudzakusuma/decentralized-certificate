a7eb73be6972b16cf15b3ab42bd25ff8c1715434
laudzakusuma <laudzaxie@gmail.com>
2025-06-06 20:12:24 +0700

    hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,15 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
+  networks: {
+    // Menambahkan konfigurasi untuk jaringan Amoy Testnet
+    amoy: {
+      url: process.env.MUMBAI_RPC_URL || "", // Kita tetap pakai variabel lama agar tidak perlu ganti di .env
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+  },
 };
