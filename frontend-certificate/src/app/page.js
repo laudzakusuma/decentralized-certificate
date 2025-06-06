e5809f333a2d66d3eec5133f7e1ff11f3acdef8b
laudzakusuma <laudzaxie@gmail.com>
2025-06-07 00:09:31 +0700

    kaciw

--- frontend-certificate/src/app/page.js
+++ frontend-certificate/src/app/page.js
@@ -6,10 +6,8 @@ import { AnimatePresence, motion } from 'framer-motion';
 // Impor semua komponen yang diperlukan
 import Header from '@/components/Header';
 import WalletStatus from '@/components/WalletStatus';
-import HomeSection from '@/components/HomeSection';
 import AdminPanel from '@/components/AdminPanel';
 import VerificationForm from '@/components/VerificationForm';
-import AboutSection from '@/components/AboutSection';
 import CertificateView from '@/components/CertificateView';
 
 export default function Home() {
@@ -24,16 +22,11 @@ export default function Home() {
 
     // Jika tidak, tampilkan section yang aktif
     switch (activeSection) {
-      case 'home':
-        return <HomeSection />;
       case 'admin':
         return <AdminPanel />;
       case 'verify':
         return <VerificationForm setCertificateData={setCertificateData} />;
-      case 'about':
-        return <AboutSection />;
       default:
-        return <HomeSection />;
     }
   };
 
