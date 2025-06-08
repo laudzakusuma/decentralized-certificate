ddd6512ac96b50fd7f032d99514ab1c17eae7386
laudzakusuma <laudzaxie@gmail.com>
2025-06-08 11:06:31 +0700

    sol

--- contracts/Certificate.sol
+++ contracts/Certificate.sol
@@ -7,28 +7,18 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
 /**
  * @title Certificate
- * @dev Kontrak untuk menerbitkan sertifikat digital sebagai Soulbound Token (SBT).
- * Soulbound berarti NFT ini tidak dapat dipindahtangankan setelah diterbitkan.
- * Hanya pemilik kontrak (institusi) yang dapat menerbitkan (mint) sertifikat baru.
+ * @dev Kontrak yang di-upgrade dengan kemampuan batch minting.
  */
 contract Certificate is ERC721, ERC721URIStorage, Ownable {
     uint256 private _nextTokenId;
 
-    /**
-     * @dev Constructor untuk menginisialisasi kontrak.
-     * @param initialOwner Alamat wallet yang akan menjadi pemilik kontrak (institusi).
-     */
     constructor(address initialOwner)
         ERC721("Decentralized Certificate", "DCRT")
         Ownable(initialOwner)
     {}
 
     /**
-     * @dev Fungsi untuk menerbitkan sertifikat baru.
-     * Hanya bisa dipanggil oleh pemilik kontrak (institusi).
-     * @param recipient Alamat wallet penerima sertifikat (mahasiswa).
-     * @param _tokenURI Link ke metadata JSON yang disimpan di IPFS.
-     * * PERBAIKAN: Mengganti nama parameter 'tokenURI' menjadi '_tokenURI' untuk menghindari shadowing.
+     * @dev Fungsi untuk menerbitkan satu sertifikat.
      */
     function safeMint(address recipient, string memory _tokenURI)
         public
@@ -40,11 +30,26 @@ contract Certificate is ERC721, ERC721URIStorage, Ownable {
     }
 
     /**
-     * @dev Override fungsi _update untuk mencegah transfer token.
-     * Ini adalah inti dari mekanisme "Soulbound Token".
-     * Fungsi ini akan gagal (revert) jika 'from' bukan alamat nol,
-     * yang berarti token hanya bisa ditransfer saat proses minting (dari 0x0 ke penerima).
-     * Setiap upaya transfer setelahnya akan dibatalkan.
+     * @dev FUNGSI BARU: Menerbitkan banyak sertifikat sekaligus.
+     * @param recipients Array alamat wallet para penerima.
+     * @param _tokenURIs Array link metadata IPFS untuk setiap sertifikat.
+     * Pastikan panjang array recipients dan _tokenURIs sama.
+     */
+    function batchMint(address[] calldata recipients, string[] calldata _tokenURIs)
+        public
+        onlyOwner
+    {
+        require(recipients.length == _tokenURIs.length, "Certificate: Array lengths do not match.");
+        
+        for (uint256 i = 0; i < recipients.length; i++) {
+            uint256 tokenId = _nextTokenId++;
+            _safeMint(recipients[i], tokenId);
+            _setTokenURI(tokenId, _tokenURIs[i]);
+        }
+    }
+
+    /**
+     * @dev Override fungsi _update untuk mencegah transfer token (Soulbound).
      */
     function _update(address to, uint256 tokenId, address auth)
         internal
@@ -56,9 +61,6 @@ contract Certificate is ERC721, ERC721URIStorage, Ownable {
         return super._update(to, tokenId, auth);
     }
 
-    // Fungsi berikut di-override dari ERC721URIStorage.
-    // Kita perlu melakukannya karena Solidity membutuhkan kejelasan ketika ada
-    // beberapa kontrak induk yang memiliki fungsi dengan nama yang sama.
     function tokenURI(uint256 tokenId)
         public
         view
